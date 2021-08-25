2fe2d97dd550d467bd4a00f33ed58dbafe9d6e77
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-25 17:21:44 +0700

    Tugas 24 Javascript
    Tugas ini kami kumpulkan Untuk Niomic

---
+++ tugas24.js
@@ -0,0 +1,8 @@
+function duaempat(){
+    var array1 = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+    console.log("Sebelumnya : ", array1);
+
+    console.log("Ascending :", array1.sort());
+    console.log("Descending : ", array1.reverse());
+}
+duaempat()
